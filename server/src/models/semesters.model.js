5cd3be0301f53e60c5f1c15636cf69ece15a4822
arafatmansuri <mansuriarafat302@gmail.com>
2025-01-18 19:54:10 +0530

    Added Semester model

---
+++ server/src/models/semesters.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+/* 
+     1.SemesterNo PK
+     2.Year
+     3.STDCount
+*/
+const semsterSchema = new Schema({
+  semsterNo: {
+    type: Number,
+    required: true,
+    unique: true,
+    enum: [1, 2, 3, 4, 5, 6],
+    index: true,
+  },
+  year: {
+    type: Date,
+    default: Date.now(),
+  },
+  studentsCount: {
+    type: Number,
+  },
+});
+
+export const Semester = mongoose.model("Semester", adminSchema);

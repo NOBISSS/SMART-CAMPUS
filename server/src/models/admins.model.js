b012e47787507df1c06648eeb0e6229d30cbac9e
arafatmansuri <mansuriarafat302@gmail.com>
2025-01-18 18:21:46 +0530

    Added Admin Model

---
+++ server/src/models/admins.model.js
@@ -0,0 +1,80 @@
+import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
+import mongoose, { Schema } from "mongoose";
+/* 
+     1.AdminId
+     2.FullName
+     3.FirstName
+     4.LastName
+     5.EmailId
+     6.Password
+*/
+const adminSchema = new Schema(
+  {
+    adminId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+
+  return next();
+});
+adminSchema.methods.generateAccessToken = async function () {
+  return jsonwebtoken.sign(
+    {
+      _id: this._id,
+      enrollmentId: this.enrollmentId,
+      email: this.emailId,
+      fullName: this.fullName,
+    },
+    process.env.JWT_ACCESS_TOKEN,
+    { expiresIn: "1d" }
+  );
+};
+adminSchema.methods.generateRefreshToken = async function () {
+  return jsonwebtoken.sign(
+    {
+      _id: this._id,
+    },
+    process.env.JWT_REFRESH_TOKEN,
+    { expiresIn: "10d" }
+  );
+};
+
+export const Admin = mongoose.model("Admin", adminSchema);

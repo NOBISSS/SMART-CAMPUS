d7d7b8017e5adb806034aeaeb882fe26d54756aa
arafatmansuri <mansuriarafat302@gmail.com>
2025-02-07 16:50:13 +0530

    Added Update and get syllbus route and contoller

--- server/src/controllers/syllabus.controller.js
+++ server/src/controllers/syllabus.controller.js
@@ -64,4 +64,152 @@ const deleteSyllabus = asyncHandler(async (req, res) => {
   }
 });
 
-export { deleteSyllabus, setSyllabus };
+const updateSyllabus = asyncHandler(async (req, res) => {
+  const syllabusId = req.params.syllabusId;
+  const { subjectName, semester } = req.body;
+  const syllabusFileLocalPath = req.file?.path;
+  if (
+    [subjectName, semester, syllabusFileLocalPath].some((field) => {
+      return field?.trim() === "";
+    })
+  )
+    throw new ApiError(400, { message: "All fields are required" });
+  let syllabusFile;
+  try {
+    syllabusFile = await uploadOnCloudinary(syllabusFileLocalPath);
+    if (!syllabusFile) {
+      throw new ApiError(404, { message: "Local path not found" });
+    }
+  } catch (error) {
+    throw new ApiError(500, {
+      message: "Someting went wrong while uploading syllabus file",
+    });
+  }
+  try {
+    const syllabus = await Syllabus.findByIdAndUpdate(
+      syllabusId,
+      {
+        subjectName: subjectName,
+        semester: _.toNumber(semester),
+        syllabusFile: syllabusFile?.secure_url,
+      },
+      { new: true }
+    );
+    if (!syllabus) {
+      throw new ApiError(400, { message: "No syllabus found" });
+    }
+    await syllabus.save({ validateBeforeSave: false });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, syllabus, "Syllabus updated successfully"));
+  } catch (error) {
+    return res
+      .status(error.statusCode)
+      .json(
+        new ApiResponse(
+          error.statusCode,
+          {},
+          error.message || "Someting went wrong while updating syllabus"
+        )
+      );
+  }
+});
+
+const checkSyllabusToAdmin = asyncHandler(async (req, res) => {
+  const semester = req.body.semester;
+  try {
+    if (semester === "")
+      throw new ApiError(400, { message: "Semester is required" });
+    const syllabusAsSemester = await Syllabus.aggregate([
+      {
+        $match: {
+          semester: _.toNumber(semester),
+        },
+      },
+      {
+        $project: {
+          subjectName: 1,
+          semester: 1,
+          syllabusFile: 1,
+          createdAt: 1,
+          updatedAt: 1,
+        },
+      },
+    ]);
+    if (!syllabusAsSemester) {
+      throw new ApiError(404, {
+        message: `No syllabus found for semester ${semester}`,
+      });
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          syllabusAsSemester,
+          `${syllabusAsSemester.length} found syllabus for semester ${semester} fetched successfully`
+        )
+      );
+  } catch (error) {
+    return res
+      .status(error.statusCode)
+      .json(
+        new ApiResponse(
+          error.statusCode,
+          {},
+          error.message || "Someting went wrong while updating syllabus"
+        )
+      );
+  }
+});
+
+const getSyllabusForStudents = asyncHandler(async (req,res) => {
+  const semester = req.user.semester;
+  if (semester === "")
+    throw new ApiError(400, { message: "Semester is required" });
+  try {
+    const syllabusAsSemester = await Syllabus.aggregate([
+      {
+        $match: {
+          semester: _.toNumber(semester),
+        },
+      },
+      {
+        $project: {
+          subjectName: 1,
+          semester: 1,
+          syllabusFile: 1,
+          createdAt: 1,
+          updatedAt: 1,
+        },
+      },
+    ]);
+    if (!syllabusAsSemester) {
+      throw new ApiError(404, {
+        message: `No syllabus found for semester ${semester}`,
+      });
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          syllabusAsSemester,
+          `${syllabusAsSemester.length} found syllabus for semester ${semester} fetched successfully`
+        )
+      );
+  } catch (error) {
+    return res
+      .status(error.statusCode)
+      .json(
+        new ApiResponse(
+          error.statusCode,
+          {},
+          error.message || "Someting went wrong while updating syllabus"
+        )
+      );
+  }
+})
+
+export { checkSyllabusToAdmin, deleteSyllabus, setSyllabus, updateSyllabus,getSyllabusForStudents };
--- server/src/routes/syllabus.route.js
+++ server/src/routes/syllabus.route.js
@@ -1,15 +1,24 @@
 import Router from "express";
 import {
+  checkSyllabusToAdmin,
   deleteSyllabus,
+  getSyllabusForStudents,
   setSyllabus,
+  updateSyllabus,
 } from "../controllers/syllabus.controller.js";
 import verifyJWTAdmin from "../middleware/adminAuth.middlewres.js";
 import { upload } from "../middleware/multer.middlewares.js";
+import verifyJWTStudent from "../middleware/studentAuth.middlewres.js";
 const router = Router();
 
 router
   .route("/create")
   .post(verifyJWTAdmin, upload.single("syllabusFile"), setSyllabus);
 router.route("/delete").delete(verifyJWTAdmin, deleteSyllabus);
+router.route("/update").patch(verifyJWTAdmin, updateSyllabus);
+router.route("/getbysemester").get(verifyJWTAdmin, checkSyllabusToAdmin);
+router
+  .route("/getbysemesterstudent")
+  .get(verifyJWTStudent, getSyllabusForStudents);
 
 export default router;
